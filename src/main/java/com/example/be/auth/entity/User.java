23516b50109ba2a607816221cae10208be6f9617
2iedo <162278702+2iedo@users.noreply.github.com>
2025-03-13 16:48:23 +0900

    feat : User 엔티티 클래스 추가

---
+++ src/main/java/com/example/be/auth/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.be.auth.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+
+    public User(String email, String username, String password){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
