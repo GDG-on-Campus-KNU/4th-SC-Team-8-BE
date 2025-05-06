ad7254ea0d3eeb0a10dd3aa8f940a765d7e99b93
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-07 00:40:56 +0900

    refactor : 무작위 20개 링크 조회 기능 최적화

--- src/main/java/com/example/be/game/repository/GameRepository.java
+++ src/main/java/com/example/be/game/repository/GameRepository.java
@@ -1,5 +1,6 @@
 package com.example.be.game.repository;
 
+import com.example.be.game.dto.GameResponse;
 import com.example.be.game.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,6 @@ import java.util.List;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     boolean existsByYoutubeLink(String youtubeLink);
-    @Query(value = "SELECT * FROM game ORDER BY RAND() LIMIT 20", nativeQuery = true)
-    List<Game> findRandomGames();
+    @Query("SELECT new com.example.be.game.dto.GameResponse(g.youtubeLink) FROM Game g ORDER BY RAND() LIMIT 20")
+    List<GameResponse> findRandomGames();
 }
