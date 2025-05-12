62cd2e5ba323157b6b2475b6d2b3254ed33288d0
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-12 13:58:03 +0900

    feat : url 기반 메일 전송 기능 추가

--- src/main/java/com/example/be/auth/dto/MailRequest.java
+++ src/main/java/com/example/be/auth/dto/MailRequest.java
@@ -1,4 +1,6 @@
 package com.example.be.auth.dto;
 
-public record MailRequest(String email) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MailRequest(@Schema(description = "유튜브 주소", example = "https://www.youtube.com/watch?v=EhJzbcbz5-s")String url) {
 }
